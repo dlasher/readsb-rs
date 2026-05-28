f148278975f89e1bdbae29e4abaa05ad6265b41a
readsb-rs Implementer <implementer@readsb-rs.dev>
2026-05-27 22:58:27 -0700

    feat(viewsb): add crossterm terminal screen management (Task 5)

--- src/viewsb/screen.rs
+++ src/viewsb/screen.rs
@@ -1,25 +1,50 @@
-use std::io;
+use crossterm::{
+    cursor::{Hide, Show},
+    event::{self, Event, KeyCode, KeyEvent},
+    execute, queue,
+    terminal::{self, Clear, ClearType, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use std::io::{self, Stdout, Write};
 
-pub struct TerminalGuard;
+pub struct TerminalGuard {
+    stdout: Stdout,
+}
 
 impl TerminalGuard {
     pub fn new() -> io::Result<Self> {
-        Ok(TerminalGuard)
+        let mut stdout = io::stdout();
+        terminal::enable_raw_mode()?;
+        queue!(stdout, EnterAlternateScreen, Hide)?;
+        stdout.flush()?;
+        Ok(TerminalGuard { stdout })
     }
 
     pub fn clear(&mut self) -> io::Result<()> {
-        Ok(())
+        queue!(
+            self.stdout,
+            Clear(ClearType::All),
+            crossterm::cursor::MoveTo(0, 0)
+        )?;
+        self.stdout.flush()
     }
 
-    pub fn write_line(&mut self, _line: &str) -> io::Result<()> {
-        Ok(())
+    pub fn write_line(&mut self, line: &str) -> io::Result<()> {
+        writeln!(self.stdout, "{}", line)
     }
 
     pub fn should_exit(&self) -> io::Result<bool> {
+        if event::poll(std::time::Duration::from_millis(0))? {
+            if let Event::Key(KeyEvent { code: KeyCode::Char('c'), .. }) = event::read()? {
+                return Ok(true);
+            }
+        }
         Ok(false)
     }
 }
 
 impl Drop for TerminalGuard {
-    fn drop(&mut self) {}
+    fn drop(&mut self) {
+        let _ = execute!(self.stdout, Show, LeaveAlternateScreen);
+        let _ = terminal::disable_raw_mode();
+    }
 }
